043874339922b31188d4ba092b042c48fd80bb0f
jhdpc <as313234@gmail.com>
2025-02-05 15:08:47 +0800

    環境建置

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import axios from "axios";
+import { Modal } from "bootstrap";
+
+function App() {
+  return (
+    <>
+      <h1>你好</h1>
+    </>
+  );
+}
+
+export default App;
