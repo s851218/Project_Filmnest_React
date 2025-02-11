1d169e3c033be24ae39229b2c788bacc1218b573
jhdpc <as313234@gmail.com>
2025-02-11 09:20:28 +0800

    常見問答按鈕調整

--- src/adminPages/AdminEdit.jsx
+++ src/adminPages/AdminEdit.jsx
@@ -1,28 +1,33 @@
 import { NavLink, Outlet } from "react-router";
 
 export default function AdminEdit() {
+  const getActiveStyle = ({ isActive }) => ({
+    backgroundColor: isActive ? "black" : "transparent",
+    color: isActive ? "white" : "black",
+    borderBottom: "none",
+  });
   return (
     <>
       <h2>專案編輯</h2>
       <div className="contaner">
         <ul className="nav nav-pills nav-fill mb-6">
           <li className="nav-item">
-            <NavLink className="nav-link" to="/admin/adminEdit/intro">
+            <NavLink className="nav-link" style={getActiveStyle} to="/admin/adminEdit/intro">
               專案資訊
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/admin/adminEdit/news">
+            <NavLink className="nav-link" style={getActiveStyle} to="/admin/adminEdit/news">
               最新消息
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/admin/adminEdit/faq">
+            <NavLink className="nav-link" style={getActiveStyle} to="/admin/adminEdit/faq">
               常見問題
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/admin/adminEdit/feedback">
+            <NavLink className="nav-link" style={getActiveStyle} to="/admin/adminEdit/feedback">
               回饋項目
             </NavLink>
           </li>
