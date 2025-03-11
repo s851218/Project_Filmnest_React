b096d2aa85b95064a1c925b823decf03b88d8459
s851218 <sin0981802858@gmail.com>
2025-03-11 16:10:53 +0800

    AdminHome RWD 調整 (解決劇照編輯異常)

--- src/adminPages/AdminHome.jsx
+++ src/adminPages/AdminHome.jsx
@@ -4,19 +4,15 @@ import AdminSidebar from "../AdminComponents/AdminSidebar";
 export default function AdminHome() {
   return (
     <>
-      <div className="d-none d-lg-block pt-8">
+      <div className="pt-3 pt-lg-8">
         <div className="row">
-          <div className="col-2">
+          <div className="col-lg-2">
             <AdminSidebar />
           </div>
-          <div className="col-10">
+          <div className="col-12 col-lg-10">
             <Outlet />
           </div>
         </div>
-      </div>
-      <div className="d-block d-lg-none pt-3">
-        <AdminSidebar />
-        <Outlet />
       </div>
     </>
   );
