15099409d9a867a4ecb24af6589b2e254ac2774b
yuki5718 <hayate8575@gmail.com>
2025-03-12 03:31:39 +0800

    新增 paymentInfo slice

---
+++ src/slice/paymentInfoSlice.js
@@ -0,0 +1,81 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const paymentInfoSlice = createSlice({
+  name: "paymentInfo",
+  initialState: {
+    userInfo: {
+      userName: "王曉明",
+      userPhone: "0912345678",
+      userEmail: "aaa@gmail.com",
+    },
+    recipientInfo: {
+      recipientName: "", 
+      recipientPhone: "", 
+      recipientEmail: "", 
+    },
+    sameAsMember: false, 
+    address: {
+
+    },
+    accordion: {
+      index: 0,
+    },
+    paymentOption: {
+
+    },
+    requried: {
+      paymentInfo: false,
+      paymentType: false,
+    }
+  },
+  reducers: {
+    setUserInfo(state, action) {
+      // 寫入會員資料
+
+    },
+    setRecipientInfo(state, action) {
+      // 寫入收件人資料
+      state.recipientInfo = action.payload
+    },
+    setAddress(state, action) {
+      // 寫入地址
+      state.address = action.payload
+    },
+    setSameAsMember(state, action) {
+      // 收件人同會員
+      if (action.payload) {
+        return {
+          ...state,
+          sameAsMember: action.payload,
+        }
+      }
+    },
+    setAccordionIndex(state, action) {
+      // 寫入當前手風琴選擇 => 付款方式(信用卡/ATM/超商代碼)
+      state.accordion.index = action.payload
+    },
+    setPaymentOption(state, action) {
+      // 寫入付款方式內容
+      state.paymentOption = action.payload
+    },
+    setRequried(state,action) {
+      console.log(action.payload)
+      const { name , value } = action.payload
+      switch (name) {
+        case "paymentInfo":
+          state.requried.paymentInfo = value
+          break;
+        case "paymentType":
+          state.requried.paymentType = value
+          break;
+      
+        default:
+          break;
+      }
+    }
+  }
+})
+
+export const { setAddress , setRecipientInfo , setSameAsMember , setAccordionIndex , setPaymentOption , setRequried } = paymentInfoSlice.actions
+
+export default paymentInfoSlice.reducer
--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slice/userSlice";
 import categoryReducer from "./slice/categorySlice";
+import paymentInfoReducer from "./slice/paymentInfoSlice"
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     category: categoryReducer,
+    paymentInfo: paymentInfoReducer,
   },
 });
