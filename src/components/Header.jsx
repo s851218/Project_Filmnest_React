6bea454e487d2e813f9a6400f9836581e7d3cbe6
jhdpc <as313234@gmail.com>
2025-03-12 16:03:01 +0800

    登入登出功能調整

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,31 @@ import { Link, NavLink, useNavigate } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
 import { setCategory } from "../slice/categorySlice";
 import { setSearchText, setSearchValue, setIsSearchOpen } from "../slice/searchSlice";
+import { setLogout } from "../slice/userSlice";
+import axios from "axios";
+const apiBase = import.meta.env.VITE_API_BASE;
 
 export default function Header() {
   const profile = useSelector((state) => state.user.profile);
+  const id = useSelector((state)=>state.user.profile.userId);
   const searchValue = useSelector((state) => state.search.value);
   const isSearchOpen = useSelector((state) => state.search.isSearchOpen);
   const categoryData = ["喜劇", "愛情", "恐怖", "懸疑", "科幻", "紀錄片", "動畫", "實驗電影"];
   const dispatch = useDispatch();
   const navbarRef = useRef(null);
   const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.patch(`${apiBase}/users/${id}`, { token: "" });
+      document.cookie = "loginToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+      dispatch(setLogout());
+      navigate("/")
+      alert("登出成功");
+    } catch (error) {
+      alert("登出失敗");
+    }
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -47,12 +63,12 @@ export default function Header() {
 
   return (
     <>
-      <nav ref={navbarRef} className="navbar navbar-expand-md navbar-dark py-5">
+      <nav ref={navbarRef} className="navbar navbar-expand-md navbar-dark py-5 d-none d-lg-flex">
         <div className="container">
           <NavLink className="p-0 me-12" to="/">
             <img src="https://github.com/s851218/Project-FilmNest/blob/main/assets/images/logo.png?raw=true" alt="logo" />
           </NavLink>
-          <Link type="button" className="navbar-toggler border-0 ms-auto" to="/headerSm">
+          <Link type="button" className="navbar-toggler border-0 ms-auto d-lg-none" to="/headerSm">
             <span className="navbar-toggler-icon"></span>
           </Link>
           <div className="collapse navbar-collapse text-white">
@@ -104,9 +120,14 @@ export default function Header() {
             </div>
             <div className="ms-auto d-flex flex-column flex-lg-row align-items-center">
               {profile.token ? (
-                <NavLink to="/personalCenter" className="btn btn-outline-light fw-bolder py-1 px-1 me-8 border-0">
-                  <span className="me-2">{profile.userName}</span> <img src={profile.imageUrl} className="rounded-circle object-fit-cover" style={{ width: "40px", height: "40px" }} alt="" />
-                </NavLink>
+                <>
+                  <button type="button" className="btn btn-primary" onClick={handleLogout}>
+                    登出
+                  </button>
+                  <NavLink to="/personalCenter" className="btn btn-outline-light fw-bolder py-1 px-1 me-8 border-0">
+                    <span className="me-2">{profile.userName}</span> <img src={profile.imageUrl} className="rounded-circle object-fit-cover" style={{ width: "40px", height: "40px" }} alt="" />
+                  </NavLink>
+                </>
               ) : (
                 <NavLink to="/login" className="btn btn-outline-light fw-bolder py-3 px-5 me-8">
                   登入 / 註冊
@@ -119,6 +140,16 @@ export default function Header() {
               </NavLink>
             </div>
           </div>
+        </div>
+      </nav>
+      <nav ref={navbarRef} className="navbar navbar-expand-md navbar-dark py-5 d-lg-flex d-lg-none">
+        <div className="container">
+          <NavLink className="p-0 me-12" to="/">
+            <img src="https://github.com/s851218/Project-FilmNest/blob/main/assets/images/logo.png?raw=true" alt="logo" />
+          </NavLink>
+          <Link type="button" className="navbar-toggler border-0 ms-auto d-block" to="/headerSm">
+            <span className="navbar-toggler-icon"></span>
+          </Link>
         </div>
       </nav>
     </>
