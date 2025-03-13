e47e222a8b6b414981b56fe77d5d0a79c3a984c9
jhdpc <as313234@gmail.com>
2025-03-13 21:55:48 +0800

    會員中心-個人資料頁面及收藏專案頁面開發

--- src/pages/FavoriteProject.jsx
+++ src/pages/FavoriteProject.jsx
@@ -1,7 +1,32 @@
-export default function FavoriteProject(){
-    return (
-        <div className="container">
-            <h1>收藏專案</h1>
-        </div>
-    )
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Card from "../components/Card";
+import PersonalCenterSidebar from "../components/PersonalCenterSidebar";
+const apiBase = import.meta.env.VITE_API_BASE;
+
+export default function FavoriteProject() {
+  const [favoriteProjects, setFavoriteProjects] = useState([]);
+  const id = useSelector((state) => state.user.profile.userId);
+
+  const getFavoriteProjects = async () => {
+    try {
+      const response = await axios.get(`${apiBase}/favorites?_expand=project&userId=${id}`);
+      setFavoriteProjects(response.data.map((item) => item.project));
+    } catch (error) {}
+  };
+  useEffect(() => {
+    getFavoriteProjects();
+  }, []);
+  return (
+    <div className="container container-lg">
+      <div className="d-block d-lg-none w-100 mb-5 bg-primary-8">
+        <PersonalCenterSidebar />
+      </div>
+      <div className="row row-lg">
+        <h1 className="fs-6 mb-5">收藏專案</h1>
+        <Card projects={favoriteProjects} isSwiper={false} />
+      </div>
+    </div>
+  );
 }
