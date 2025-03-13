4728858bb42e15418124456468f5b6929ea2b5c8
jhdpc <as313234@gmail.com>
2025-03-13 21:52:44 +0800

    DB修改時間戳，時間顯示邏輯調整

--- src/pages/ProjectIntroNews.jsx
+++ src/pages/ProjectIntroNews.jsx
@@ -11,6 +11,19 @@ export default function ProjectIntroNews() {
   const newsCollapseRef = useRef([]);
   const newsCollapseInstances = useRef([]);
 
+  const getTime = (time) =>{
+    const newTime = new Date(time).toLocaleString("zh-TW", {
+      timeZone: "Asia/Taipei",
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    }).replace(/\//g, "-")
+    return newTime
+  }
+
   useEffect(() => {
     (async () => {
       try {
@@ -54,7 +67,7 @@ export default function ProjectIntroNews() {
               <div className="row mb-3" key={item.id}>
                   <div className="col-10 mx-auto">
                       <button class="btn btn-primary w-100 fs-5 d-flex justify-content-between" type="button" onClick={() => handleCollapse(item.id)}>
-                        {item.title} {(postsIsOpen[index].isOpen) ? <i class="bi bi-chevron-up"></i> : <i class="bi bi-chevron-down"></i>}
+                        <span>{item.title}</span> <span className="d-flex align-items-center"><span className="fs-base me-2">{getTime(item.date)}</span>{(postsIsOpen[index].isOpen) ? <i class="bi bi-chevron-up"></i> : <i class="bi bi-chevron-down"></i>}</span>
                       </button>
                     <div class="collapse " ref={(el) => (newsCollapseRef.current[index] = el)}>
                       <div class="card card-body">{item.content}</div>
