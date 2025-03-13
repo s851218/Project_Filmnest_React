81ac6cc4881c9ed772aa71b6886485f927470f24
yuki5718 <hayate8575@gmail.com>
2025-03-14 01:35:11 +0800

    修改計算bonus的state

--- src/components/BonusCalculator.jsx
+++ src/components/BonusCalculator.jsx
@@ -1,36 +1,27 @@
-import { useEffect } from "react"
-import { useForm, useWatch } from "react-hook-form"
+import { useForm } from "react-hook-form"
 
-export default function BonusCalculator ({ originPrice , totalPrice , setTotalPrice }) {
-  const { register , reset , control , handleSubmit } = useForm({defaultValues:{customized:""}})
+export default function BonusCalculator ({ bonus , setBonse }) {
+  const { register , reset , handleSubmit } = useForm({defaultValues:{customized:""}})
 
   // 處理按鈕金額
   const handleAddBonus = (e) => {
-    const addBouns = Number(e.target.value)
-    const newPrice = totalPrice + addBouns
-    setTotalPrice(newPrice)
+    const addBonus = Number(e.target.value)
+    const newBonus = bonus + addBonus
+    setBonse(newBonus)
   }
 
   // 處理重製
-  const handleResetBonus = (e) => {
-    const resetPrice = Number(e.target.value)
-    setTotalPrice(resetPrice)
+  const handleResetBonus = () => {
+    setBonse(0)
   }
   
   // 處理自訂金額
   const onsubmit = (data) => {
     const addBouns = Number(data.customized)
-    const newPrice = totalPrice + addBouns
-    setTotalPrice(newPrice)
+    const newBonus = bonus + addBouns
+    setBonse(newBonus)
     reset()
   }
-
-  // 表單監控
-  const watch = useWatch({control})
-  useEffect(()=>{
-    console.log(watch);
-  },[watch])
-
 
   return (
     <>
@@ -42,7 +33,7 @@ export default function BonusCalculator ({ originPrice , totalPrice , setTotalPr
       <div className="input-group mb-4">
         <input type="text" name="customized" id="customized" placeholder="自訂金額" className="form-control rounded-start-1" {...register("customized")} />
         <button type="button" className="btn btn-primary border-white" id="addCustomAmount" onClick={handleSubmit(onsubmit)}>加碼</button>
-        <button type="button" className="btn btn-secondary rounded-end-1" id="resetCustomAmount" value={originPrice} onClick={(e) => handleResetBonus(e)}>重設</button>
+        <button type="button" className="btn btn-secondary rounded-end-1" id="resetCustomAmount" onClick={handleResetBonus}>重設</button>
       </div>
       
     </>
