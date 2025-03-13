4d86163a32cfa6c39cafbfa42b39c61e74b754f5
yuki5718 <hayate8575@gmail.com>
2025-03-13 23:33:38 +0800

    移除不需要的程式碼

--- src/pages/ProjectIntroSupportFeedback.jsx
+++ src/pages/ProjectIntroSupportFeedback.jsx
@@ -2,10 +2,6 @@ import { useSelector } from "react-redux";
 import FeedbackSwiper from "../components/FeedbackSwiper";
 
 export default function ProjectIntroSupportFeedback() {
-  const userId = useSelector((state) => state.user.profile.userId);
-  const selected = useSelector((state) => state.paymentInfo.selected);
-
-  console.log("userId", userId, "selected", selected);
   return (
     <>
       <FeedbackSwiper />
