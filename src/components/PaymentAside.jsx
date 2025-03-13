c13c7e0df3ad66eb9d21ca9ae3bbb46f33b4c7e0
yuki5718 <hayate8575@gmail.com>
2025-03-14 01:52:47 +0800

    修正取得資料的位置

--- src/components/PaymentAside.jsx
+++ src/components/PaymentAside.jsx
@@ -1,84 +1,41 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router"
-const API_BASE = import.meta.env.VITE_API_BASE;
-
-export default function PaymentAside ({handleFormsSubmit}) {
-  const { id } = useParams()
-  const [ orderData , setOrderData ] = useState({})
-  const [ projectData , setProjectData ] = useState({})
-  const [ productData , setProductData ] = useState({})
-
-  // 取得訂單資料
-  useEffect(()=>{
-    const getOrder = async (id) => {
-      try {
-        const res = await axios.get(`${API_BASE}/orders?orderId=${id}`)
-        setOrderData(res.data[0])
-      } catch (error) {
-        console.log("訂單資料取得錯誤",error);
-      }
-    }
-    if (id) {
-      getOrder(id)
-    }
-  },[id])
-  
-  // 取得專案資料
-  const getData = async(order) => {
-    const { projectId , productId } = order
-    try {
-      const projectRes = await axios.get(`${API_BASE}/projects?projectId=${projectId}`)
-      const productRes = await axios.get(`${API_BASE}/products?productId=${productId}`)
-      setProjectData(projectRes.data[0])
-      setProductData(productRes.data[0])
-    } catch (error) {
-      console.log("資料取得錯誤",error);
-    }
-  }
-
-  useEffect(()=>{
-    if (Object.keys(orderData).length !== 0) {
-      getData(orderData)
-    }
-  },[orderData])
+export default function PaymentAside ({
+  handleFormsSubmit,
+  orderData,
+  projectData,
+  productData,
+}) {
 
   return (
-    <>
-      {(Object.keys(orderData).length !== 0) && (Object.keys(projectData).length !== 0) && (Object.keys(productData).length !== 0) && (
-        <aside className="col-lg-4 d-lg-block d-none">
-          <div className="card checkout-info-sidebar bg-primary-9 border rounded-1 h-auto" style={{border: `1px sold #606060`}}>
-            <div className="order-info">
-              <div className="card-header p-4">
-                <h3 className="mb-5">訂單資訊</h3>
-                <h4 className="fs-7 fw-normal mb-3">{projectData.projectTitle}</h4>
-                <h4 className="fs-7 fw-normal mb-3">{`【${productData.title}】`}</h4>
-              </div>
-              <div className="card-body">
-                <ul className="list-unstyled">
-                  <li className="d-flex justify-content-between align-items-center mb-3">
-                    <h5 className="small text-primary-2 mb-0">訂單編號：</h5>
-                    <p className="mb-0 font-monospace">{orderData.orderId}</p>
-                  </li>
-                  <li className="d-flex justify-content-between align-items-center mb-5">
-                    <h5 className="small text-primary-2 mb-0">訂單金額：</h5>
-                    <p className="mb-0">NT$ {orderData?.totalPrice.toLocaleString()}</p>
-                  </li>
-                  <hr />
-                  <li className="d-flex justify-content-between align-items-center mb-5">
-                    <h5 className="fs-7 text-primary-2 mb-0">應付金額：</h5>
-                    <p className="mb-0 fw-bold fs-7">NT$ <span className="total-number">{orderData?.totalPrice.toLocaleString()}</span></p>
-                  </li>
-                </ul>
-              </div>
-              <div className="card-footer">
-                <button type="button" className="btn btn-primary w-100 my-2" onClick={handleFormsSubmit}>確認付款</button>
-              </div>
-            </div>
+    <aside className="col-lg-4 d-lg-block d-none">
+      <div className="card checkout-info-sidebar bg-primary-9 border rounded-1 h-auto" style={{border: `1px sold #606060`}}>
+        <div className="order-info">
+          <div className="card-header p-4">
+            <h3 className="mb-5">訂單資訊</h3>
+            <h4 className="fs-7 fw-normal mb-3">{projectData.projectTitle}</h4>
+            <h4 className="fs-7 fw-normal mb-3">{`【${productData.title}】`}</h4>
           </div>
-        </aside>
-      )}
-    </>
-
+          <div className="card-body">
+            <ul className="list-unstyled">
+              <li className="d-flex justify-content-between align-items-center mb-3">
+                <h5 className="small text-primary-2 mb-0">訂單編號：</h5>
+                <p className="mb-0 font-monospace">{orderData.orderId}</p>
+              </li>
+              <li className="d-flex justify-content-between align-items-center mb-5">
+                <h5 className="small text-primary-2 mb-0">訂單金額：</h5>
+                <p className="mb-0">NT$ {orderData?.totalPrice.toLocaleString()}</p>
+              </li>
+              <hr />
+              <li className="d-flex justify-content-between align-items-center mb-5">
+                <h5 className="fs-7 text-primary-2 mb-0">應付金額：</h5>
+                <p className="mb-0 fw-bold fs-7">NT$ <span className="total-number">{orderData?.totalPrice.toLocaleString()}</span></p>
+              </li>
+            </ul>
+          </div>
+          <div className="card-footer">
+            <button type="button" className="btn btn-primary w-100 my-2" onClick={handleFormsSubmit}>確認付款</button>
+          </div>
+        </div>
+      </div>
+    </aside>
   )
 }
--- src/pages/PaymentInfo.jsx
+++ src/pages/PaymentInfo.jsx
@@ -1,9 +1,13 @@
-import { useEffect, useRef } from "react";
+import axios from "axios";
+import { useEffect, useState , useRef } from "react";
+import { useParams } from "react-router"
 import { useSelector } from "react-redux";
 import PaymentAside from "../components/PaymentAside";
 import PaymentMobileFooter from "../components/PaymentMobileFooter";
 import PaymentInfoFrom from "../components/PaymentInfoFrom";
 import PaymentCollapseFrom from "../components/PaymentCollapseFrom";
+
+const API_BASE = import.meta.env.VITE_API_BASE;
 
 export default function PaymentInfo() {
   // 路由跳轉頁面時，重製滾輪捲軸
@@ -12,6 +16,45 @@ export default function PaymentInfo() {
     document.documentElement.style.scrollBehavior = "auto";
     window.scrollTo(0, 0);
   }, []);
+
+  const { id } = useParams()
+  const [ orderData , setOrderData ] = useState({})
+  const [ projectData , setProjectData ] = useState({})
+  const [ productData , setProductData ] = useState({})
+
+  // 取得訂單資料
+  useEffect(()=>{
+    const getOrder = async (id) => {
+      try {
+        const res = await axios.get(`${API_BASE}/orders?orderId=${id}`)
+        setOrderData(res.data[0])
+      } catch (error) {
+        console.log("訂單資料取得錯誤",error);
+      }
+    }
+    if (id) {
+      getOrder(id)
+    }
+  },[id])
+  
+  // 取得專案資料
+  const getData = async(order) => {
+    const { projectId , productId } = order
+    try {
+      const projectRes = await axios.get(`${API_BASE}/projects?projectId=${projectId}`)
+      const productRes = await axios.get(`${API_BASE}/products?productId=${productId}`)
+      setProjectData(projectRes.data[0])
+      setProductData(productRes.data[0])
+    } catch (error) {
+      console.log("資料取得錯誤",error);
+    }
+  }
+
+  useEffect(()=>{
+    if (Object.keys(orderData).length !== 0) {
+      getData(orderData)
+    }
+  },[orderData])
 
   const infoFromRef = useRef();
   const paymentFromRef = useRef();
@@ -23,25 +66,34 @@ export default function PaymentInfo() {
   };
   const userProfile = useSelector((state) => state.user.profile);
 
-  
+
   return (
     <>
-      <div className="container mb-20" style={{ marginTop: 88 }}>
-        <div className="row">
-          <main className="col-lg-8">
-            {/* 付款資料 V */}
-            <h2 className="fs-lg-3 fs-4 text-primary-2 mb-4">付款資料</h2>
-            <PaymentInfoFrom reference={infoFromRef} />
-            {/* 付款方式 V */}
-            <h2 className="fs-lg-3 fs-4 text-primary-2 mb-4">付款方式</h2>
-            <PaymentCollapseFrom reference={paymentFromRef} />
-          </main>
-
-          <PaymentAside handleFormsSubmit={handleFormsSubmit} />
-        </div>
-      </div>
-
-      <PaymentMobileFooter handleFormsSubmit={handleFormsSubmit} />
+      {(Object.keys(orderData).length !== 0) && (Object.keys(projectData).length !== 0) && (Object.keys(productData).length !== 0) ? (
+        <>
+          <div className="container mb-20" style={{ marginTop: 88 }}>
+            <div className="row">
+              <main className="col-lg-8">
+                {/* 付款資料 V */}
+                <h2 className="fs-lg-3 fs-4 text-primary-2 mb-4">付款資料</h2>
+                <PaymentInfoFrom reference={infoFromRef} />
+                {/* 付款方式 V */}
+                <h2 className="fs-lg-3 fs-4 text-primary-2 mb-4">付款方式</h2>
+                <PaymentCollapseFrom reference={paymentFromRef} />
+              </main>
+    
+              <PaymentAside
+                handleFormsSubmit={handleFormsSubmit}
+                orderData={orderData}
+                projectData={projectData}
+                productData={productData}
+              />
+            </div>
+          </div>
+    
+          <PaymentMobileFooter handleFormsSubmit={handleFormsSubmit} />
+        </>
+      ) : (<div className="vh-100"></div>)}
     </>
   );
 }
