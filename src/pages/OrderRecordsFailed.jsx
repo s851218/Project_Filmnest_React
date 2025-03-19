9ad691791af8608aa529a8f76947946d15d10635
jhdpc <as313234@gmail.com>
2025-03-19 15:59:00 +0800

    訂單管理頁面開發

---
+++ src/pages/OrderRecordsFailed.jsx
@@ -0,0 +1,5 @@
+export default function OrderRecordsFailed(){
+    return(<>
+      <h1 className="fs-6">成功紀錄</h1>
+    </>)
+}
---
+++ src/pages/OrderRecordsUnpaid.jsx
@@ -0,0 +1,5 @@
+export default function OrderRecordsUnpaid(){
+    return(<>
+      <h1 className="fs-6">未付款訂單紀錄</h1>
+    </>)
+}
