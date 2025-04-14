f774b495c5f55dbfe76a364ad07c2fe263410a29
yuki5718 <hayate8575@gmail.com>
2025-04-14 20:25:14 +0800

    整合reducer

--- src/slice/paymentInfoSlice.js
+++ src/slice/paymentInfoSlice.js
@@ -29,10 +29,6 @@ const paymentInfoSlice = createSlice({
       const { type , data } = action.payload
       state[type] = data
     },
-    setAccordionIndex(state, action) {
-      // 寫入當前手風琴選擇 => 付款方式(信用卡/ATM/超商代碼)
-      state.accordion.index = action.payload
-    },
     setPaymentOption(state, action) {
       // 寫入付款方式內容
       state.paymentOption = action.payload
@@ -40,6 +36,6 @@ const paymentInfoSlice = createSlice({
   }
 })
 
-export const { setPaymentInfo , setAccordionIndex , setPaymentOption } = paymentInfoSlice.actions
+export const { setPaymentInfo , setPaymentOption } = paymentInfoSlice.actions
 
 export default paymentInfoSlice.reducer
