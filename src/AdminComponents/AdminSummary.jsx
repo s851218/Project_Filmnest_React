4a81d9a394f3b6cc8679cbcbac7a384ddf70434f
s851218 <sin0981802858@gmail.com>
2025-05-18 15:56:48 +0800

    fix: 更改專案收藏數名稱

--- src/AdminComponents/AdminSummary.jsx
+++ src/AdminComponents/AdminSummary.jsx
@@ -1,17 +1,25 @@
-export default function AdminSummary () {
-  return(
+export default function AdminSummary() {
+  return (
     <div className="container mb-6 mb-lg-15">
       <h1 className="fs-6 mb-3 mb-sm-7">摘要</h1>
       <div className="summary">
         <div className="item shadow ">
-          <img src="fundsRaisedIcon.png" className="bg-primary-5" alt="fundsRaisedIcon" />
+          <img
+            src="fundsRaisedIcon.png"
+            className="bg-primary-5"
+            alt="fundsRaisedIcon"
+          />
           <div>
             <h3 className="fs-base mb-0 text-primary-6">募資金額</h3>
             <p className="fs-md-3 fs-7 fw-bolder mb-0">5,511,654</p>
           </div>
         </div>
         <div className="item shadow">
-          <img src="watchNumIcon.png" className="bg-primary-5" alt="watchNumIcon" />
+          <img
+            src="watchNumIcon.png"
+            className="bg-primary-5"
+            alt="watchNumIcon"
+          />
           <div>
             <h3 className="fs-base mb-0 text-primary-6">觀看次數</h3>
             <p className="fs-md-3 fs-7 fw-bolder mb-0">5.3K</p>
@@ -25,7 +33,11 @@ export default function AdminSummary () {
           </div>
         </div>
         <div className="item shadow">
-          <img src="projectsNumIcon.png" className="bg-primary-5" alt="projectsNumIcon" />
+          <img
+            src="projectsNumIcon.png"
+            className="bg-primary-5"
+            alt="projectsNumIcon"
+          />
           <div>
             <h3 className="fs-sm mb-0 text-primary-6">專案數量</h3>
             <p className="fs-md-6 fs-7 fw-bolder mb-0">4</p>
@@ -39,23 +51,36 @@ export default function AdminSummary () {
           </div>
         </div>
         <div className="item shadow">
-          <img src="collectNumIcon.png" className="bg-primary-5" alt="collectNumIcon" />
+          <img
+            src="collectNumIcon.png"
+            className="bg-primary-5"
+            alt="collectNumIcon"
+          />
           <div>
-            <h3 className="fs-sm mb-0 text-primary-6">collectNumIcon</h3>
+            <h3 className="fs-sm mb-0 text-primary-6">專案收藏數</h3>
             <p className="fs-md-6 fs-7 fw-bolder mb-0">672</p>
           </div>
         </div>
         <div className="item shadow">
-          <img src="watchTimeIcon.png" className="bg-primary-5" alt="watchTimeIcon" />
+          <img
+            src="watchTimeIcon.png"
+            className="bg-primary-5"
+            alt="watchTimeIcon"
+          />
           <div>
             <h3 className="fs-sm mb-0 text-primary-6">觀看時間</h3>
-            <p className="fs-md-6 fs-7 fw-bolder mb-0">368
+            <p className="fs-md-6 fs-7 fw-bolder mb-0">
+              368
               <span className="fs-base ms-2">hr</span>
             </p>
           </div>
         </div>
         <div className="item shadow">
-          <img src="commitNumIcon.png" className="bg-primary-5" alt="commitNumIcon" />
+          <img
+            src="commitNumIcon.png"
+            className="bg-primary-5"
+            alt="commitNumIcon"
+          />
           <div>
             <h3 className="fs-sm mb-0 text-primary-6">留言數</h3>
             <p className="fs-md-6 fs-7 fw-bolder mb-0">368</p>
@@ -63,5 +88,5 @@ export default function AdminSummary () {
         </div>
       </div>
     </div>
-  )
+  );
 }
