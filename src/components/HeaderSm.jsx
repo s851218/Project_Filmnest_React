0d5153bdeaf6aa16283812c1576f66cf15d4f9f9
s851218 <sin0981802858@gmail.com>
2025-05-23 23:28:50 +0800

    feat: 調整探索下拉選單 icon 圖片顯示

--- src/components/HeaderSm.jsx
+++ src/components/HeaderSm.jsx
@@ -12,7 +12,16 @@ export default function HeaderSm() {
   const profile = useSelector((state) => state.user.profile);
   const id = useSelector((state) => state.user.profile.userId);
   const isSearchOpen = useSelector((state) => state.search.isSearchOpen);
-  const categoryData = ["喜劇", "愛情", "恐怖", "懸疑", "科幻", "紀錄片", "動畫", "實驗電影"];
+  const categoryData = [
+    { comedyIcon: "喜劇" },
+    { romanceIcon: "愛情" },
+    { horrorIcon: "恐怖" },
+    { suspendIcon: "懸疑" },
+    { sciFiIcon: "科幻" },
+    { documentalIcon: "紀錄片" },
+    { animeIcon: "動畫" },
+    { experimentalFilmIcon: "實驗電影" },
+  ];
   const [isExpand, setIsExpand] = useState({ category: false });
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -20,7 +29,8 @@ export default function HeaderSm() {
   const handleLogout = async () => {
     try {
       await axios.patch(`${apiBase}/users/${id}`, { token: "" });
-      document.cookie = "loginToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+      document.cookie =
+        "loginToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
       Toast.fire({
         icon: "success",
         title: "登出成功",
@@ -54,11 +64,14 @@ export default function HeaderSm() {
     <>
       <div className="py-3 d-flex justify-content-between container">
         <div>
-          <Link className="me-3" onClick={(e)=>handleReturnPage(e)}>
+          <Link className="me-3" onClick={(e) => handleReturnPage(e)}>
             <img src="close_white.png" alt="close" style={{ width: "20px" }} />
           </Link>
           <Link to="/">
-            <img src="https://github.com/s851218/Project-FilmNest/blob/main/assets/images/logo.png?raw=true" alt="logo" />
+            <img
+              src="https://github.com/s851218/Project-FilmNest/blob/main/assets/images/logo.png?raw=true"
+              alt="logo"
+            />
           </Link>
         </div>
         <div className="nav-item">
@@ -72,16 +85,26 @@ export default function HeaderSm() {
               }}
             >
               <span className="fw-bolder me-2 text-white">搜尋</span>
-              <span className="material-symbols-outlined text-white">search</span>
+              <span className="material-symbols-outlined text-white">
+                search
+              </span>
             </button>
           </div>
         </div>
       </div>
       <ul className="nav flex-column">
         {profile.token ? (
-          <Link to="/headerSmSec" className="d-flex justify-content-between align-items-center fw-bolder btn btn-primary-8 btn-main border-0 border-bottom">
+          <Link
+            to="/headerSmSec"
+            className="d-flex justify-content-between align-items-center fw-bolder btn btn-primary-8 btn-main border-0 border-bottom"
+          >
             <div>
-              <img src={profile.imageUrl} className="rounded-circle object-fit-cover" style={{ width: "40px", height: "40px" }} alt="" />
+              <img
+                src={profile.imageUrl}
+                className="rounded-circle object-fit-cover"
+                style={{ width: "40px", height: "40px" }}
+                alt=""
+              />
               <span className="ms-2">{profile.userName}</span>
             </div>
             <div>
@@ -89,27 +112,66 @@ export default function HeaderSm() {
             </div>
           </Link>
         ) : (
-          <Link to="/login" className="btn btn-primary-8 btn-main border-0 border-bottom fw-bolder">
+          <Link
+            to="/login"
+            className="btn btn-primary-8 btn-main border-0 border-bottom fw-bolder"
+          >
             登入 / 註冊
           </Link>
         )}
-        <Link className="nav-item btn-main border-bottom" onClick={() => handleIsExpand()}>
+        <Link
+          className="nav-item btn-main border-bottom"
+          onClick={() => handleIsExpand()}
+        >
           探索
         </Link>
         {isExpand.category && (
           <>
             <li>
-              <Link className="dropdown-item py-2 px-5" to="/projectExplore" onClick={() => dispatch(setCategory("all"))} onMouseEnter={(e) => (e.target.style.backgroundColor = "rgba(255, 255, 255, 0.3)")} onMouseLeave={(e) => (e.target.style.backgroundColor = "transparent")}>
-                <img src="allProjectsIcon.png" alt="allProjectsIcon" className="me-2" style={{ width: "24px" }} />
-                allProjectsIcon
+              <Link
+                className="dropdown-item py-2 px-5"
+                to="/projectExplore"
+                onClick={() => dispatch(setCategory("all"))}
+                onMouseEnter={(e) =>
+                  (e.target.style.backgroundColor = "rgba(255, 255, 255, 0.3)")
+                }
+                onMouseLeave={(e) =>
+                  (e.target.style.backgroundColor = "transparent")
+                }
+              >
+                <img
+                  src="allProjectsIcon.png"
+                  alt="allProjectsIcon"
+                  className="me-2"
+                  style={{ width: "24px" }}
+                />
+                全部專案
               </Link>
             </li>
             {categoryData.map((item, index) => {
+              const key = Object.keys(item)[0];
+              const value = Object.values(item)[0];
               return (
                 <li key={index}>
-                  <Link className="dropdown-item py-2 px-5" to="/projectExplore" onClick={() => dispatch(setCategory(item))} onMouseEnter={(e) => (e.target.style.backgroundColor = "rgba(255, 255, 255, 0.3)")} onMouseLeave={(e) => (e.target.style.backgroundColor = "transparent")}>
-                    <img src={`${item}.png`} alt={item} className="me-2" style={{ width: "24px" }} />
-                    {item}
+                  <Link
+                    className="dropdown-item py-2 px-5"
+                    to="/projectExplore"
+                    onClick={() => dispatch(setCategory(value))}
+                    onMouseEnter={(e) =>
+                      (e.target.style.backgroundColor =
+                        "rgba(255, 255, 255, 0.3)")
+                    }
+                    onMouseLeave={(e) =>
+                      (e.target.style.backgroundColor = "transparent")
+                    }
+                  >
+                    <img
+                      src={`${key}.png`}
+                      alt={value}
+                      className="me-2"
+                      style={{ width: "24px" }}
+                    />
+                    {value}
                   </Link>
                 </li>
               );
@@ -117,12 +179,22 @@ export default function HeaderSm() {
           </>
         )}
         {profile.hasStudio && (
-          <Link to="/admin/adminProfile" className={`nav-item mt-3 btn btn-primary btn-main border-0 fw-bolder ${!profile.token ? "d-none" : ""}`}>
+          <Link
+            to="/admin/adminProfile"
+            className={`nav-item mt-3 btn btn-primary btn-main border-0 fw-bolder ${
+              !profile.token ? "d-none" : ""
+            }`}
+          >
             工作室
           </Link>
         )}
-        <Link className="nav-item mt-3 btn btn-primary btn-main border-0 fw-bolder">我要提案</Link>
-        <Link className="nav-item mt-3 btn btn-primary btn-main border-0 fw-bolder" onClick={handleLogout}>
+        <Link className="nav-item mt-3 btn btn-primary btn-main border-0 fw-bolder">
+          我要提案
+        </Link>
+        <Link
+          className="nav-item mt-3 btn btn-primary btn-main border-0 fw-bolder"
+          onClick={handleLogout}
+        >
           登出
         </Link>
       </ul>
