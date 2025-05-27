cee1181ab2f6fc57e83d9199338407c198cd79c2
s851218 <sin0981802858@gmail.com>
2025-05-27 17:40:34 +0800

    feat: 將原本 AboutContent.jsx 元件拆分成三個元件，會根據 NavLink 顯示相對應內容 調整內容： 1. 新增 AboutStudioInProgress.jsx 元件，為「進行中」專案內容 2. 新增 AboutStudioInFinished.jsx 元件，為「已結束」專案內容 3. 新增 AboutStudioIn.jsx 元件，為「其他作品集」內容

---
+++ src/pages/aboutStudio/AboutStudioInProgress.jsx
@@ -0,0 +1,28 @@
+import { useOutletContext } from "react-router";
+import Card from "../../components/Card";
+import GrayScreenLoading from "../../components/GrayScreenLoading";
+
+export default function AboutStudioInProgress() {
+  const { projectsData, isLoading } = useOutletContext();
+
+  return (
+    <>
+      <div className="container py-5 py-lg-20">
+        {projectsData.length !== 0 ? (
+          <div className="row">
+            <Card projects={projectsData.projects} isSwiper={false} />
+          </div>
+        ) : (
+          <div className="row justify-content-center">
+            <div className="col-8 bg-primary-6 rounded-3">
+              <div className="d-flex justify-content-center py-15">
+                <h2 className="fs-base fs-lg-6">尚未有進行中專案</h2>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <GrayScreenLoading isLoading={isLoading} />
+    </>
+  );
+}
