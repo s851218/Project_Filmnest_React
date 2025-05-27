24eb15f24be459dd5429794c49fb0697e36ee83d
s851218 <sin0981802858@gmail.com>
2025-05-27 20:55:13 +0800

    feat: 調整 Navbar 的 hover 與 active 顯示邏輯、NavLink 路由調整 調整內容： 1.  Navbar 的 hover 與 active 顯示改用 CSS 控制 2. NavLink 的 to 路由調整，以正確顯示 active 效果 3. async function 改放在 useLayoutEffect(因為畫面會閃爍)

--- src/pages/aboutStudio/AboutStudio.jsx
+++ src/pages/aboutStudio/AboutStudio.jsx
@@ -1,91 +1,84 @@
-import { useCallback, useEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import axios from "axios";
-import { Link, Outlet, ScrollRestoration, useParams } from "react-router";
+import { NavLink, Outlet, ScrollRestoration, useParams } from "react-router";
 const apiBase = import.meta.env.VITE_API_BASE;
 import { Helmet } from "react-helmet-async";
 import GrayScreenLoading from "../../components/GrayScreenLoading";
 import { Alert } from "../../js/customSweetAlert";
 
 export default function AboutStudio() {
-  const [hoveredIndex, setHoveredIndex] = useState(null);
   const navItems = ["進行中", "已結案", "其他作品集"];
+
   const [projectsData, setProjectsData] = useState({
     projects: [],
     finProjects: [],
     length: 0,
     finLength: 0,
   });
+
   const [isLoading, setIsLoading] = useState(false);
+
   const { id } = useParams();
 
   // 提案者資料
   const [studios, setStudios] = useState({});
+
   // 取得提案者資料
-  const getStudioProfile = useCallback(async () => {
-    setIsLoading(true);
-    let idArray = id.split("=");
-    let idObj = { [idArray[0]]: Number(idArray[1]) };
-    const { projectId } = idObj;
-    try {
-      const res = await axios.get(
-        `${apiBase}/projects?_expand=studio&id=${projectId}`
-      );
-      setStudios(res.data[0].studio);
-    } catch (error) {
-      if (error) {
-        Alert.fire({
-          icon: "error",
-          title: "取得提案者專案失敗",
-        });
+  useLayoutEffect(() => {
+    const getStudioProfile = async () => {
+      setIsLoading(true);
+      let idArray = id.split("=");
+      let idObj = { [idArray[0]]: Number(idArray[1]) };
+      const { projectId } = idObj;
+      try {
+        const res = await axios.get(
+          `${apiBase}/projects?_expand=studio&id=${projectId}`
+        );
+        setStudios(res.data[0].studio);
+      } catch (error) {
+        if (error) {
+          Alert.fire({
+            icon: "error",
+            title: "取得提案者專案失敗",
+          });
+        }
+      } finally {
+        setIsLoading(false);
       }
-    } finally {
-      setIsLoading(false);
-    }
+    };
+
+    getStudioProfile();
   }, [id]);
 
   // 取得專案資料
-  const getProjectData = useCallback(async () => {
-    setIsLoading(true);
-    try {
-      const response = await axios.get(
-        `${apiBase}/projects?studioId=${studios.userId}`
-      );
-      const finFilterData = response.data.filter((item) => item.isFin);
-      const filterData = response.data.filter((item) => !item.isFin);
-      setProjectsData({
-        projects: filterData,
-        finProjects: finFilterData,
-        length: filterData.length,
-        finLength: finFilterData.length,
-      });
-    } catch (error) {
-      if (error) {
+  useLayoutEffect(() => {
+    const getProjectData = async () => {
+      if (!studios.userId) return;
+      setIsLoading(true);
+      try {
+        const response = await axios.get(
+          `${apiBase}/projects?studioId=${studios.userId}`
+        );
+        const finFilterData = response.data.filter((item) => item.isFin);
+        const filterData = response.data.filter((item) => !item.isFin);
+        setProjectsData({
+          projects: filterData,
+          finProjects: finFilterData,
+          length: filterData.length,
+          finLength: finFilterData.length,
+        });
+      } catch (error) {
+        console.log("取得專案資料失敗：", error);
         Alert.fire({
           icon: "error",
           title: "取得專案資料失敗",
         });
+      } finally {
+        setIsLoading(false);
       }
-    } finally {
-      setIsLoading(false);
-    }
+    };
+    getProjectData();
   }, [studios.userId]);
-  useEffect(() => {
-    if (studios.userId) {
-      getProjectData();
-    }
-  }, [studios.userId, getProjectData]);
-
-  // 路由跳轉至專案介紹頁時，重製滾輪捲軸
-  useEffect(() => {
-    // 將滾動行為設為 auto 避免有捲動過程的動畫
-    document.documentElement.style.scrollBehavior = "auto";
-    window.scrollTo(0, 0);
-  }, []);
-
-  // 元件初始化
-  useEffect(() => {
-    getStudioProfile();
-  }, [getStudioProfile]);
 
   const { studioProfile } = studios;
 
@@ -99,26 +92,25 @@ export default function AboutStudio() {
       )}
       {studioProfile && (
         <>
-          <div className="container mt-20 pb-15">
+          <div className="container mt-0 mt-md-6 pb-15">
             <div className="row justify-content-center">
               <div className="col-xl-8 col-lg-10">
                 <div className="border-bottom border-primary-4 pb-8 mb-8">
                   <div className="row flex-md-row flex-column">
-                    <div className="col-lg-4 col-md-5 text-center">
+                    <div className="col-md-5">
                       <img
                         src={studioProfile.studioImageUrl}
-                        className="img-fluid object-fit-cover img-director"
-                        style={{ height: 264, width: 264 }}
+                        className="img-fluid object-fit-cover rounded-md w-100 h-100 mb-5 mb-md-0"
                         alt={studioProfile.groupName}
                       />
                     </div>
-                    <div className="col-lg-8 col-md-7">
+                    <div className="col-md-7">
                       <div className="d-flex flex-column h-100">
-                        <h3 className="fs-6 fs-md-4 mb-3">
+                        <h3 className="fs-6 fs-md-4 fs-lg-3 mb-3 text-nowrap">
                           {studioProfile.groupName}
                         </h3>
-                        <p className="fs-sm">
-                          <i className="bi bi-person" />
+                        <p className="mb-5 mb-md-10">
+                          <i className="bi bi-person fs-7 me-1" />
                           {studioProfile.personResponsible}
                         </p>
                         <div
@@ -129,22 +121,22 @@ export default function AboutStudio() {
                             border: "1px solid #606060",
                           }}
                         >
-                          <div className="d-flex justify-content-between mb-2">
+                          <div className="d-flex justify-content-between mb-2 align-items-center">
                             <h3 className="fs-sm mb-0">聯絡我們</h3>
-                            <ul className="list-unstyled d-flex align-self-end mb-0">
+                            <ul className="list-unstyled d-flex align-self-end mb-0 gap-2">
                               <li>
                                 <a href={studioProfile.studioFb}>
-                                  <i className="bi bi-facebook fs-base social-media-hover"></i>
+                                  <i className="bi bi-facebook fs-7 social-media-hover"></i>
                                 </a>
                               </li>
                               <li className="mx-2">
                                 <a href={studioProfile.studioIg}>
-                                  <i className="bi bi-instagram fs-base social-media-hover"></i>
+                                  <i className="bi bi-instagram fs-7 social-media-hover"></i>
                                 </a>
                               </li>
                               <li>
                                 <a href={studioProfile.studioLine}>
-                                  <i className="bi bi-line fs-base social-media-hover"></i>
+                                  <i className="bi bi-line fs-7 social-media-hover"></i>
                                 </a>
                               </li>
                             </ul>
@@ -152,20 +144,20 @@ export default function AboutStudio() {
 
                           <div className="d-flex justify-content-between">
                             <ul className="list-unstyled mb-0">
-                              <li className="fs-sm mb-1">
+                              <li className="mb-1">
                                 <a href={`tel:${studioProfile.phone}`}>
-                                  <i className="bi bi-telephone" />{" "}
+                                  <i className="bi bi-telephone fs-7 me-1" />{" "}
                                   {studioProfile.phone}
                                 </a>
                               </li>
-                              <li className="fs-sm mb-1">
+                              <li className="mb-1">
                                 <a href={studioProfile.email}>
-                                  <i className="bi bi-envelope" />{" "}
+                                  <i className="bi bi-envelope fs-7 me-1" />{" "}
                                   {studioProfile.email}
                                 </a>
                               </li>
-                              <li className="fs-sm">
-                                <i className="bi bi-clock" />{" "}
+                              <li>
+                                <i className="bi bi-clock fs-7 me-1" />{" "}
                                 {studioProfile.startTime}~
                                 {studioProfile.endTime}
                               </li>
@@ -179,10 +171,9 @@ export default function AboutStudio() {
 
                 <div id="teamIntro">
                   <h3 className="fs-7 mb-3 d-flex align-items-center">
-                    <span className="me-2">
-                      。{studioProfile.personResponsible}
-                    </span>
-                    <span className="fs-sm fw-normal">導演/攝影</span>
+                    <span className="aboutStudio-title--decoration me-3"></span>
+                    {studioProfile.personResponsible}
+                    <span className="fs-sm fw-normal ms-2">導演/攝影</span>
                   </h3>
                   <p className="mb-0">{studioProfile.teamIntro}</p>
                 </div>
@@ -190,52 +181,31 @@ export default function AboutStudio() {
             </div>
           </div>
           <nav>
-            <ul className="nav bg-primary-6 justify-content-center">
+            <ul className="nav bg-primary-8 py-3 justify-content-center gap-6 gap-md-8">
               {navItems.map((item, index) => (
                 <li className="nav-item border-0" key={index}>
-                  <Link
-                    className="nav-link link-primary-1 text-center border-0 position-relative"
+                  <NavLink
+                    className={({ isActive }) =>
+                      `nav-link--aboutStudio text-center ${
+                        isActive ? "nav-link-aboutStudio--active" : ""
+                      }`
+                    }
                     to={`${
                       item === "進行中"
-                        ? "aboutStudioOngoing"
+                        ? ""
                         : item === "已結案"
-                        ? "aboutStudioFin"
+                        ? "aboutStudioFinished"
                         : "aboutStudioOthers"
                     }`}
-                    style={{
-                      backgroundColor: "transparent",
-                      overflow: "hidden",
-                    }}
+                    end={item === "進行中"}
                   >
-                    <span
-                      style={{
-                        position: "relative",
-                        paddingBottom: "5px",
-                        overflow: "hidden",
-                      }}
-                      onMouseEnter={() => setHoveredIndex(index)}
-                      onMouseLeave={() => setHoveredIndex(null)}
-                    >
-                      {item}
-                      {item === "進行中"
-                        ? `(${projectsData.length})`
-                        : item === "已結案"
-                        ? `(${projectsData.finLength})`
-                        : ""}
-                      <span
-                        className="underline-effect"
-                        style={{
-                          position: "absolute",
-                          left: 0,
-                          bottom: 0,
-                          height: "2px",
-                          width: hoveredIndex === index ? "100%" : "0%",
-                          backgroundColor: "rgba(255, 255, 255, 0.8)",
-                          transition: "width 0.3s ease-in-out",
-                        }}
-                      ></span>
-                    </span>
-                  </Link>
+                    {item + " "}
+                    {item === "進行中"
+                      ? `(${projectsData.length})`
+                      : item === "已結案"
+                      ? `(${projectsData.finLength})`
+                      : ""}
+                  </NavLink>
                 </li>
               ))}
             </ul>
